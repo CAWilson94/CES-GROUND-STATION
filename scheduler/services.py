9f80bdf71d235ff09a78b8c2328cc39869798f49
Adam Kidd <ksb12177@uni.strath.ac.uk>
2016-12-15 21:22:06 +0000

    not added

--- scheduler/services.py
+++ scheduler/services.py
@@ -3,33 +3,35 @@ from scheduler.models import TLE
 
 class services():
 
+	def updateTLE():
+		requestsObject = requests.get("http://celestrak.com/NORAD/elements/cubesat.txt")
+		tle = requestsObject.text
+		splitTLE = tle.split('\r\n')
+
+		# print (splitTLE)
+		# while len(splitTLE) != 0:
+		# 	if splitTLE[0]!='':
+		# 		namesep = []
+		# 		while splitTLE[0]!="1":
+		# 			namesep.append(splitTLE.pop(0))
+					
+		# 		name = services.adder(namesep)
+
+		# 		line1sep = splitTLE[:9]
+		# 		del splitTLE[:9]
+		# 		line1 = services.adder(line1sep)
+
+		# 		line2sep = splitTLE[:8]
+		# 		del splitTLE[:8]
+		# 		line2 = services.adder(line2sep)
+
+		# 		newTLE = TLE(name=name,line1=line1,line2=line2)
+		# 		newTLE.save()
+
+		return (splitTLE)
+
 	def adder(stringsep):
 		string=""
 		for x in stringsep:
 			string = string+x+" "
 		return (string)
-
-	def updateTLE():
-		requestsObject = requests.get("http://celestrak.com/NORAD/elements/cubesat.txt")
-		tle = requestsObject.text
-		splitTLE = tle.split(" ")
-
-		while len(splitTLE) != 0:
-			namesep = []
-			while splitTLE[0]!="1":
-				namesep.append(splitTLE.pop(0))
-				
-			name = adder(namesep)
-
-			line1sep = splitTLE[:9]
-			del splitTLE[:9]
-			line1 = adder(line1sep)
-
-			line2sep = splitTLE[:8]
-			del splitTLE[:8]
-			line2 = adder(line2sep)
-
-			newTLE = TLE(name=name,line1=line1,line2=line2)
-			newTLE.save()
-
-
--- scheduler/views.py
+++ scheduler/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from scheduler.services import services
 from scheduler.models import TLE
+import requests
 
 def index(request):
 
 	#sessionObject = requests.Sessions()
 	#sessionObject.mount('https://celestrak.com',myAdapter())
 	#sessionObject.get...
-	services.updateTLE()
+	tle_list = services.updateTLE()
 	
-	tle_list = TLE.objects.all()
+	#tle_list = TLE.objects.all()
+	print (tle_list)
 	string = "Display satellites"
-	context_dict = {'tle_list':tle_list,
+	context_dict = {'message':tle_list,
 				}
 
 	return render(request, 'scheduler/index.html', context_dict)
