31726f2765c6cc5721a7550154204dcb3760b843
Adam Kidd CESM2012 <ksb12177@uni.strath.ac.uk>
2016-12-25 01:23:00 +0000

    Update services.py coping changes from commits d520ab0c70da62035493b2048c280b6d0ca32d2b & c22c7575e8a2a0cb9c8c2f6c6aebca782ef98b29

--- scheduler/services.py
+++ scheduler/services.py
@@ -3,58 +3,43 @@ from scheduler.models import TLE
 
 class services():
 
+	#Retrieving TLE data from external source and placing in the db 
 	def updateTLE():
 		requestsObject = requests.get("http://celestrak.com/NORAD/elements/cubesat.txt")
-		tle=0
-		tle = requestsObject.text
+		tle=requestsObject.text
 		
 		#check format is as expected?
-		splitByBothList = services.splitTLE(tle)
-		
-		final = services.filterTLE(splitByBothList)
+
+		#split leaves one list with format
+		#name, line1, line2, name, line1, line2
+		final = tle.split('\r\n')
+	
+		#remove errant empty entry
+		if final[len(final)-1]=='':
+		 	del final[len(final)-1] 
 
 		i=0
 		while i != (len(final)-3):
-			#print (final)
+			name = services.adder(final[i]).strip()
 			try: 
-				tleentry = TLE.objects.get(name=final[i])
+				tleentry = TLE.objects.get(name=name)
 				pass
-			except TLE.DoesNotExist:
+			except TLE.DoesNotExist as e:
 				#create new entry in db
-				newTLE = TLE(name = final[i], line1 = final[i+1], line2 = final[i+2])
+				newTLE = TLE(name =name, line1 = final[i+1], line2 = final[i+2])
 				newTLE.save()
 			else:
 				#update existing
 				tleentry.line1 = final[i+1]
 				tleentry.line2 = final[i+2]
+				tleentry.save()
 			i+=3
-
-		return (final)
+		
+		
 
 	#add up name or line array to single space delimited string
 	def adder(stringsep):  #nicer way?
 		string=""
 		for x in stringsep:
-			string = string+x+" "
+			string = string+x
 		return (string)
-
-	def splitTLE(rawTLE):
-		splitrnTLEList = rawTLE.split('\r\n')
-
-		cookedTLE = []
-		for x in splitrnTLEList:
-			cookedTLE.append(x.split(" "))
-		return (cookedTLE)
-
-	def filterTLE(rawTLE):
-				#filter out the empty entries caused by splitting '       '
-		newLineOrNameList=[]
-		filteredTLE=[]
-		for lineOrNameList in rawTLE:
-			newLineOrNameList = [lineOrName for lineOrName in lineOrNameList if lineOrName!='']
-			filteredTLE.append(services.adder(newLineOrNameList))
-
-		#delete odd empty end entry 
-		if filteredTLE[len(filteredTLE)-1]=='':
-			del filteredTLE[len(filteredTLE)-1]  #nicer way?
-		return (filteredTLE)
