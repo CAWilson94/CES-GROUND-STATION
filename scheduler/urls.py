433ab5b438004af10905b274d307aadef0fe89f4
Adam Kidd <ksb12177@uni.strath.ac.uk>
2017-01-13 14:06:56 +0000

    gitignore __pycache__/* doesn't work

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -4,10 +4,9 @@ from scheduler import views
 
 urlpatterns = [
 	#url(r'^updateTLE/$',views.TLEUpdate.as_view()),
-	url(r'^scheduler/$', views.TLEList.as_view()),
+	url(r'^', views.TLEList.as_view()),
+	url(r'^scheduler/?$', views.TLEList.as_view()),
 	url(r'^scheduler/(?P<pk>[0-9]+)/?$', views.TLEDetail.as_view()),
-	#url(r'^', views.TLEList.as_view()),
-
 ]#this order matters
 
 urlpatterns = format_suffix_patterns(urlpatterns)
