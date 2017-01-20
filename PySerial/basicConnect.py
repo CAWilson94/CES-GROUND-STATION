dda544c3c29809e716017e5aa3b6165274985f76
Baldeep <baldeepsingh.93@gmail.com>
2017-01-20 16:08:27 +0000

    azimuth elevation rotators moving - yaldi

--- PySerial/basicConnect.py
+++ PySerial/basicConnect.py
@@ -4,17 +4,28 @@ import serial
 import time
 
 
-ser = serial.Serial("COM2", 9600, timeout=10)  # open serial port: windows "COMN" linux "/dev/ttyUSBn" where n is a number
-ser.flushInput()
-ser.flushOutput()
+ser = serial.Serial("COM2", 9600, timeout=2)  # open serial port: windows "COMN" linux "/dev/ttyUSBn" where n is a number
+
 print(ser)
 
 print("writing to COM2")
 
-cmd = "2AG30\r\n".encode('ascii')
+cmd = "2BG60\n".encode('ascii')
 print("command: ", cmd)
+ser.flush()
 
-ser.write(cmd)
+
+
+ser.write("\x02".encode())
+ser.write("B".encode())
+ser.write("G400".encode())
+ser.write("\r".encode())
+
+ser.write("\x02".encode())
+ser.write("A".encode())
+ser.write("G00".encode())
+ser.write("\r".encode())
+
 
 
 ser.portstr
@@ -24,11 +35,15 @@ print("Done writing")
 print("Reading response")
 
 line = ser.read() 
-#line.portstr
-print(line)
+
+while(line != b''):
+	print(line)
+	line=ser.read()
+
+
 
 print("done")
-time.sleep(10)
+
 ser.close()
 
 
