69f9af10dd3aba6bb8a6dd14b20e3ea4da4052a2
Charlotte Wilson <charlotteawilson.94@gmail.com>
2017-01-23 08:16:13 +0000

    function refactoring

--- scheduler/RotatorServices.py
+++ scheduler/RotatorServices.py
@@ -1,4 +1,6 @@
 import requests
+import serial
+import time
 
 """
  Set and set the position of the ground station rotators: azimuth and elevation
@@ -7,8 +9,15 @@ import requests
 
 class rotator_services():
 
-	# get position of ground station
+	ser = serial.Serial("COM2", 9600, timeout=2)  # open serial port: windows "COMN" 
+												  # linux "/dev/ttyUSBn" where n is a number
+	
 	def get_position():
+		""" Get position of ground station: 
+			azimuth and elevation
+		"""
 
-	# set position of ground station
-	def set_position():
+	def set_position(az, el):
+		""" Set position of ground station, 
+			given azimuth and elevation
+		"""
