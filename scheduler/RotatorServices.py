f0cb138d95c7494e14bfa1310e660d21448fe1dd
Charlotte Wilson <charlotteawilson.94@gmail.com>
2017-01-23 08:10:13 +0000

    rotator position getters and setters

---
+++ scheduler/RotatorServices.py
@@ -0,0 +1,14 @@
+import requests
+
+"""
+ Set and set the position of the ground station rotators: azimuth and elevation
+
+"""
+
+class rotator_services():
+
+	# get position of ground station
+	def get_position():
+
+	# set position of ground station
+	def set_position():
