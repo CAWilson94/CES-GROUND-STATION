69f3a43e27ede789f0db5dc319e4bb9965ffae2c
Baldeep <baldeepsingh.93@gmail.com>
2017-02-02 10:52:11 +0000

    Refactored from class to module, fixed integer input bug

--- scheduler/RotatorServices.py
+++ scheduler/RotatorServices.py
@@ -1,4 +1,5 @@
 import serial
+import datetime as dt
 """
 Created: 23/01/2017
 Last modified: 23/01/2017
@@ -17,77 +18,83 @@ Last modified: 23/01/2017
 	***	May also not need this in a class format: seems like modules and functions are good enough here. ***
 """
 
-class rotator_services():
+#class rotator_services():
 
+"""
+Command format: 
+	Elevation: 	"2BG<degrees>\r"  
+	Azimuth: 	"2AG<degrees>\r"
+"""
+cmdArr = ["B","A","G"]
+cmdStart = "\x02"
+cmdEnd = "\r"
+
+def write_elevation(el, ser):
 	"""
-	Command format: 
-		Elevation: 	"2BG<degrees>\r"  
-		Azimuth: 	"2AG<degrees>\r"
+	Write elevation commands for combi track
+	using command Array, given elevation and serial port
+
+	Not sure if serial port will work in this way: 
+	If not, rebase to last commmit
 	"""
-	cmdArr = ["B","A","G"]
-	cmdStart = "\x02"
-	cmdEnd = "\r"
+	ser.write(cmdStart.encode())
+	ser.write(cmdArr[0].encode())
+	ser.write((cmdArr[-1] + str(el)).encode())
+	ser.write(cmdEnd.encode())
 
-	def write_elevation(el, ser):
-		"""
-		Write elevation commands for combi track
-		using command Array, given elevation and serial port
+def write_azimuth(az, ser):
+	"""
+	Write azimuth commands for combi track
+	using command Array, given azimuth and serial port
 
-		Not sure if serial port will work in this way: 
-		If not, rebase to last commmit
-		"""
-		ser.write(cmdStart.encode())
-		ser.write(cmdArr[0].encode())
-		ser.write((cmdArr[-1] + el).encode())
-		ser.write(cmdEnd.encode())
+	Not sure if serial port will work in this way: 
+	If not, rebase to last commmit
+	"""
+	ser.write(cmdStart.encode())
+	ser.write(cmdArr[1].encode())
+	ser.write((cmdArr[-1] + str(az)).encode())
+	ser.write(cmdEnd.encode())
 
-	def write_azimuth(az, ser):
-		"""
-		Write azimuth commands for combi track
-		using command Array, given azimuth and serial port
 
-		Not sure if serial port will work in this way: 
-		If not, rebase to last commmit
-		"""
-		ser.write(cmdStart.encode())
-		ser.write(cmdArr[1].encode())
-		ser.write((cmdArr[-1] + az).encode())
-		ser.write(cmdEnd.encode())
-	
-	
-	def get_position():
-		""" Get position of ground station: 
-			azimuth and elevation
-		"""
-		ser = serial.Serial("COM2", 9600, timeout=2)  # open serial port: windows "COMN" 
-												  # linux "/dev/ttyUSBn" where n is a number
-		ser.portstr										  
-		line = ser.read() 
+def get_position():
+	""" Get position of ground station: 
+		azimuth and elevation
+	"""
+	ser = serial.Serial("COM2", 9600, timeout=2)  # open serial port: windows "COMN" 
+											  # linux "/dev/ttyUSBn" where n is a number
+	ser.portstr										  
+	line = ser.read() 
 
-		while(line != b''):
-			print(line)
-			line=ser.read()
+	while(line != b''):
+		print(line)
+		line=ser.read()
 
-		print("done")
-		ser.close()
+	print("done")
+	ser.close()
 
-	def set_position(az, el):
-		""" Set position of ground station, 
-			given azimuth and elevation
-		"""
-		ser = serial.Serial("COM2", 9600, timeout=2)  # open serial port: windows "COMN" 
-												  # linux "/dev/ttyUSBn" where n is a number
-		print(ser) # Baud rate info etc: for testing 
-		print("writing to COM2")
-		ser.flush()
-		# Change elevation first
-		rotator_services.write_elevation(el, ser)
-		# Change azimuth
-		rotator_services.write_azimuth(az, ser)
-		print("done")
-		ser.close()
+def set_position(az, el):
+	""" Set position of ground station, 
+		given azimuth and elevation
+	"""
+	ser = serial.Serial("COM2", 9600, timeout=2)  # open serial port: windows "COMN" 
+											  # linux "/dev/ttyUSBn" where n is a number
+	print(ser) # Baud rate info etc: for testing 
+	print("writing to COM2")
+	ser.flush()
+	# Change elevation first
+	write_elevation(el, ser)
+	# Change azimuth
+	write_azimuth(az, ser)
+	print("done")
+	ser.close()
 
-	
+start = dt.datetime.now()		
+set_position(0, 0)
+get_position()
+end = dt.datetime.now()
+
+time = end-start
+print(time)
 
 
 
