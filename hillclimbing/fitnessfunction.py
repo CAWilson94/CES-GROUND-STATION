b83fd10f33a9ba16a62b28b3cd58e4e628079dd5
Adam Kidd <ksb12177@uni.strath.ac.uk>
2017-02-03 14:39:15 +0000

    hill climbing works for some limited cases, shuffle could be used for random restart

--- hillclimbing/fitnessfunction.py
+++ hillclimbing/fitnessfunction.py
@@ -1,5 +1,6 @@
 #!usr/bin/env python  
 from datetime import date, datetime, timedelta
+from random import shuffle
 
 class satellite(object):
 	name=""
@@ -25,6 +26,7 @@ def fitnessFunction(satList):
 
 	satListConflictGroups = findConflictingGroups(satList)
 
+
 	mergedGroups = mergeLists(satListConflictGroups)
 
 	reorderedConflictGroups=[]
@@ -33,10 +35,10 @@ def fitnessFunction(satList):
 		reordered= [x for x in satList if x in group]
 		reorderedConflictGroups.append(reordered)
 
-	nextPass = findSchedulableSatellites(reorderedConflictGroups)
+	score = findSchedulableSatellites(reorderedConflictGroups)
 
-	print(nextPass)
-	return nextPass
+	print(score)
+	return score
 
 def findConflictingGroups(satList):
 	satListConflicts=[]
@@ -70,11 +72,7 @@ def mergeLists(satListConflicts):
 	# 					['sat7', 'sat8', 'sat9', 'sat6'], ['sat8', 'sat9', 'sat7'], 
 	# 					['sat8', 'sat9'], ['sat11', 'sat10']]
 	#print(satListConflictssatListConflicts)
-
-	
-	
 	prevSatConlicts = []
-
 	while len(satListConflicts) != len(prevSatConlicts):
 		finaListsConflictsTrimmed=[]
 		finaListsConflicts = []
@@ -171,6 +169,7 @@ def findSchedulableSatellites(satListConflictGroups):
 				thisSatPassEnd = sat.AOS + transactionTime
 				tempTime = [thisSatPassStart,thisSatPassEnd]
 				#print("bad times {}".format(tempTime))
+				scheduledSats.append(sat)
 				blackList.append(tempTime)	
 			if conflicts is False:
 				#Sat doesn't conflict with any times made so far
@@ -202,7 +201,7 @@ def findSchedulableSatellites(satListConflictGroups):
 	for satList in unScheduledSats:
 		score +=len(satList)
 	print(score) # want lowest.
-	return nextPassList
+	return score
 
 def test_findSchedulableSatellites_many_real_sats():
 
@@ -287,8 +286,6 @@ def test_findSchedulableSatellites_many_real_sats():
 
 # clas = fit.FitnessFunction()
 # clas.test_findSchedulableSatellites_many_sats()
-
-test_findSchedulableSatellites_many_sats()
 
 
 def test_findSchedulableSatellites_many_fake_sats():
@@ -377,10 +374,170 @@ def test_findSchedulableSatellites_many_fake_sats_but_diff():
 
 #check we don't lose a sat during processing
 #check against a varied set of sats
+# test_findSchedulableSatellites_many_real_sats()
+# test_findSchedulableSatellites_many_fake_sats_but_diff()
+# test_findSchedulableSatellites_many_fake_sats()
+
+
+def hillclimbing(satList):
+
+	maxIterations = 100
+	i=0
+	oldUnscheduled = 190
+	newOrder=[]
+	curOrder=satList
+	while i<maxIterations:
+		shuffle(curOrder) 
+		#shuffling can make it find different solutions
+		#shuffling count as hc with random restart?
+		newUnscheduled = fitnessFunction(curOrder)
+		if newUnscheduled < oldUnscheduled:
+			#use that 
+			print("New Order")
+			#curOrder=newOrder
+			oldUnscheduled=newUnscheduled
+			i=0
+		else:
+			print("Keep Order")
+			i+=1
+			
+	if i==100:
+		print("{} curOrder could be global maxima".format(curOrder))		
+		return curOrder
+
+
+def test_hillclimbing_many_real_sats():
+	sat1AOS = datetime(2017,1,25,12,2,0)
+	sat1LOS = datetime(2017,1,25,12,5,0)
+	sat2AOS = datetime(2017,1,25,12,0,0)
+	sat2LOS = datetime(2017,1,25,12,8,0)
+	sat3AOS = datetime(2017,1,25,12,25,0)
+	sat3LOS = datetime(2017,1,25,12,30,0)
+	sat4AOS = datetime(2017,1,25,11,57,0)
+	sat4LOS = datetime(2017,1,25,12,3,0)
+	sat5AOS = datetime(2017,1,25,12,7,0)
+	sat5LOS = datetime(2017,1,25,12,10,0)
+	sat6AOS = datetime(2017,1,25,11,57,0)
+	sat6LOS = datetime(2017,1,25,12,4,0)
+	sat7AOS = datetime(2017,1,25,11,57,0)
+	sat7LOS = datetime(2017,1,25,12,4,0)
+	sat8AOS = datetime(2017,1,25,11,57,0)
+	sat8LOS = datetime(2017,1,25,12,4,0)
+	sat9AOS = datetime(2017,1,25,13,0,0)
+	sat9LOS = datetime(2017,1,25,13,4,0)
+	sat10AOS = datetime(2017,1,25,12,27,0)
+	sat10LOS = datetime(2017,1,25,12,31,0)
+	sat11AOS = datetime(2017,1,25,12,28,0)
+	sat11LOS = datetime(2017,1,25,12,34,0)
+
+	sat1 = satellite("sat1",sat1AOS, sat1LOS)
+	sat2 = satellite("sat2",sat2AOS, sat2LOS)
+	sat3 = satellite("sat3",sat3AOS, sat3LOS)
+	sat4 = satellite("sat4",sat4AOS, sat4LOS)
+	sat5 = satellite("sat5",sat5AOS, sat5LOS)
+	sat6 = satellite("sat6",sat6AOS, sat6LOS)
+	sat7 = satellite("sat7",sat7AOS, sat7LOS)
+	sat8 = satellite("sat8",sat8AOS, sat8LOS)
+	sat9 = satellite("sat9",sat9AOS, sat9LOS)
+	sat10 = satellite("sat10",sat10AOS,sat10LOS)
+	sat11 = satellite("sat11",sat11AOS,sat11LOS)
+
+	satList=[sat1,sat2,sat3,sat4,sat5,sat6,sat7,sat8,sat9,sat10,sat11]
+
+	hillclimbing(satList)
+
+def test_hillclimbing_many_fake_sats():
+
+	sat1AOS = datetime(2017,1,25,12,2,0)
+	sat1LOS = datetime(2017,1,25,12,5,0)
+	sat2AOS = datetime(2017,1,25,12,0,0)
+	sat2LOS = datetime(2017,1,25,12,8,0)
+	sat3AOS = datetime(2017,1,25,12,25,0)
+	sat3LOS = datetime(2017,1,25,12,30,0)
+	sat4AOS = datetime(2017,1,25,11,57,0)
+	sat4LOS = datetime(2017,1,25,12,3,0)
+	sat5AOS = datetime(2017,1,25,12,7,0)
+	sat5LOS = datetime(2017,1,25,12,10,0)
+	sat6AOS = datetime(2017,1,25,11,57,0)
+	sat6LOS = datetime(2017,1,25,12,4,0)
+	sat7AOS = datetime(2017,1,25,11,57,0)
+	sat7LOS = datetime(2017,1,25,12,4,0)
+	sat8AOS = datetime(2017,1,25,11,57,0)
+	sat8LOS = datetime(2017,1,25,12,4,0)
+	sat9AOS = datetime(2017,1,25,11,57,0)
+	sat9LOS = datetime(2017,1,25,12,4,0)
+	# sat10AOS = datetime(2017,1,25,12,27,0)
+	# sat10LOS = datetime(2017,1,25,12,31,0)
+	# sat11AOS = datetime(2017,1,25,12,28,0)
+	# sat11LOS = datetime(2017,1,25,12,34,0)
+
+	sat1 = satellite("sat1",sat1AOS, sat1LOS)
+	sat2 = satellite("sat2",sat2AOS, sat2LOS)
+	sat3 = satellite("sat3",sat3AOS, sat3LOS)
+	sat4 = satellite("sat4",sat4AOS, sat4LOS)
+	sat5 = satellite("sat5",sat5AOS, sat5LOS)
+	sat6 = satellite("sat6",sat6AOS, sat6LOS)
+	sat7 = satellite("sat7",sat7AOS, sat7LOS)
+	sat8 = satellite("sat8",sat8AOS, sat8LOS)
+	sat9 = satellite("sat9",sat9AOS, sat9LOS)
+	# sat10 = satellite("sat10",sat10AOS,sat10LOS)
+	# sat11 = satellite("sat11",sat11AOS,sat11LOS)
+
+	satList=[sat1,sat2,sat3,sat4,sat5,sat6,sat7,sat8,sat9]#,sat10,sat11]
+
+	hillclimbing(satList)
+
+def test_hillclimbing_many_fake_sats_but_diff():
+
+	sat1AOS = datetime(2017,1,25,12,2,0)
+	sat1LOS = datetime(2017,1,25,12,5,0)
+	sat2AOS = datetime(2017,1,25,12,0,0)
+	sat2LOS = datetime(2017,1,25,12,8,0)
+	sat3AOS = datetime(2017,1,25,12,25,0)
+	sat3LOS = datetime(2017,1,25,12,30,0)
+	sat4AOS = datetime(2017,1,25,11,57,0)
+	sat4LOS = datetime(2017,1,25,12,3,0)
+	sat5AOS = datetime(2017,1,25,12,7,0)
+	sat5LOS = datetime(2017,1,25,12,10,0)
+	sat6AOS = datetime(2017,1,25,11,57,0)
+	sat6LOS = datetime(2017,1,25,12,4,0)
+	sat7AOS = datetime(2017,1,25,11,57,0)
+	sat7LOS = datetime(2017,1,25,12,4,0)
+	sat8AOS = datetime(2017,1,25,12,59,0)
+	sat8LOS = datetime(2017,1,25,13,3,0)
+	sat9AOS = datetime(2017,1,25,13,0,0)
+	sat9LOS = datetime(2017,1,25,13,4,0)
+	sat10AOS = datetime(2017,1,25,12,27,0)
+	sat10LOS = datetime(2017,1,25,12,31,0)
+	sat11AOS = datetime(2017,1,25,12,28,0)
+	sat11LOS = datetime(2017,1,25,12,34,0)
+
+	sat1 = satellite("sat1",sat1AOS, sat1LOS)
+	sat2 = satellite("sat2",sat2AOS, sat2LOS)
+	sat3 = satellite("sat3",sat3AOS, sat3LOS)
+	sat4 = satellite("sat4",sat4AOS, sat4LOS)
+	sat5 = satellite("sat5",sat5AOS, sat5LOS)
+	sat6 = satellite("sat6",sat6AOS, sat6LOS)
+	sat7 = satellite("sat7",sat7AOS, sat7LOS)
+	sat8 = satellite("sat8",sat8AOS, sat8LOS)
+	sat9 = satellite("sat9",sat9AOS, sat9LOS)
+	sat10 = satellite("sat10",sat10AOS,sat10LOS)
+	sat11 = satellite("sat11",sat11AOS,sat11LOS)
+
+	satList=[sat1,sat2,sat3,sat4,sat5,sat6,sat7,sat8,sat9,sat10,sat11]
+
+	hillclimbing(satList)
+
+
+test_hillclimbing_many_fake_sats()
 
 
 
-
-
+# def swap(satList):
+# 	int x1=0
+# 	int x2=0
+# 	while x1==x2:
+# 		x1 = satList
+# 	swap two random elements?
 
 
