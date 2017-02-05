36b421c3d667ddf5ce2e46193a812575b2bfa63c
ek12139 <ekb12139@uni.strath.ac.uk>
2017-02-05 16:26:00 +0000

    Sorting conflicts

--- scheduler/TLEServices.py
+++ scheduler/TLEServices.py
@@ -7,16 +7,13 @@ class TLE_Services():
 	def findTLEById(id):
 		try:
 			tleEntryId = TLE.objects.get(id = id)
-			#print(repr(tleEntryId))
 		except TLE.DoesNotExist as e:
 			tleEntryId = None
 		return tleEntryId
 
 	def findTLEByName(name):
-		tleEntryName = TLE.objects.get(name = name)
 		try:
 			tleEntryName = TLE.objects.get(name = name)
-			print(repr(tleEntryName))
 		except TLE.DoesNotExist as e:
 			tleEntryName = None
 		return tleEntryName
@@ -24,21 +21,15 @@ class TLE_Services():
 	def saveTLE(TLEw):
 		try: 
 			TLEw.save()
-			#tleEntry = TLE.objects.get(name=TLEw.name)
-			pass #what does pass do?
+			pass
 		except TLE.DoesNotExist as e:
-			print("Already exists")			
-		#else:
-			#newTLE = TLE(name=TLEw.name, line1=TLEw.line1, line2=TLEw.line2)
-			#newTLE.save()
+			print("Unable To Save")			
 
 	def removeTLEById(id):
 		try:
 			TLE.objects.get(id = id).delete()
-			#tleEntryR.delete()
-			print ("got it")
 			pass
 		except TLE.DoesNotExist as e:
-			print ("major error") #TODO: raisemassive error
+			print ("Unable to remove from db") #TODO: raisemassive error
 
 	
