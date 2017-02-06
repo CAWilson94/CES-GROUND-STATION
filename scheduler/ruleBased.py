3a4074518c60dd5aab9364740c1f0bff0aff33b1
Baldeep Singh <Baldeep Singh>
2017-02-06 16:00:04 +0000

    started picking out conflicting sats

--- scheduler/ruleBased.py
+++ scheduler/ruleBased.py
@@ -13,6 +13,8 @@ passes = [
 	Pass(2, 5, 3), Pass(14, 17, 3), Pass(26, 29, 3), 
 ]
 
+
+
 print("Initial list")
 for aPass in passes: 
 	print(str(aPass.start) + ",")
@@ -22,3 +24,30 @@ passes.sort(key=lambda x: x.start, reverse=False)
 print("Sorted list")
 for aPass in passes: 
 	print(str(aPass.start) + ",")
+
+i = 0
+print("passes: " + str(len(passes)))
+for i in range(len(passes)):
+	j = i + 1
+	print("i: " + str(i) + ", j: " + str(j))
+	conflicting = []
+	while(conflicts(passes[i], passes[j])):
+		print("i: " + str(i) + ", j: " + str(j))
+		conflicting.append(passes[j])
+		if(j < i - 2):
+			j += 1
+		else:
+			break
+	if(conflicting):
+		conflicting.append(passes[i])
+
+def conflicts(passA, passB):
+	if(passB.start>passA.start and passB.start < passA.end):
+		return False
+	else:
+		return True
+	
+def sortConflics(conflicting):
+	print("There were : " + str(len(conflicting)) + " conflics found:")
+	for aPass in conflicting: 
+		print(str(aPass.start) + " - " + str(aPass.end))
