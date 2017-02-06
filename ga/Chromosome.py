e8f1297a7227e6384efd58613e3a01faaeb776f6
Charlotte Wilson <charlotteawilson.94@gmail.com>
2017-02-06 15:16:41 +0000

    laying the foundation

---
+++ ga/Chromosome.py
@@ -0,0 +1,20 @@
+class Chromosome(object):
+
+	fitness = 0
+	chromosomeStr = null
+
+	def __init__(self,chromosomeStr,fitness):
+		self.chromosomeStr = chromosomeStr
+		self.fitness = fitness
+
+	# Can I have more than one initalizer in python??
+
+	def randomChromo():
+	"""
+	Generate a random chromsome 
+	"""
+
+	def randomGene():
+	"""
+	Generate a random gene
+	"""
