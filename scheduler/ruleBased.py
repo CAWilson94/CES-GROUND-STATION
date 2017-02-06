050f7fcf7bb718a2239d45fddc3355b343d24cf5
Baldeep Singh <Baldeep Singh>
2017-02-06 11:58:11 +0000

    made a list of passes, not much work

---
+++ scheduler/ruleBased.py
@@ -0,0 +1,14 @@
+
+class Pass():
+	def __init__(self, start, end, duration)
+	start = start
+	end = end
+	duration = duration
+
+	
+passes = [
+Pass(10, 12, 2), Pass(22, 24, 2), Pass(34, 36, 2), 
+Pass(0, 1, 1), Pass(4, 1, 1), Pass(7, 1, 1), 
+Pass(4, 6, 2), Pass(16, 18, 2), Pass(28, 30, 2), 
+Pass(2, 5, 3), Pass(14, 17, 3), Pass(26, 29, 3), 
+]
