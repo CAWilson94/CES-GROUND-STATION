5cc31225a89e9fc3d69c0c08d0010f3be2becd99
Charlotte Wilson <charlotteawilson.94@gmail.com>
2017-02-06 15:16:41 +0000

    laying the foundation

---
+++ ga/ga.py
@@ -0,0 +1,28 @@
+from . hillclimbing import fitnessfunction as f
+
+
+f.fitnessFunction(satList)
+
+
+
+def populationFitness(String target, List<String> population)
+"""
+Evaluates current fitness of the generation
+"""
+
+def crossover(Chromosome parentOne, Chromosome parentTwo)
+"""
+Crossover two given chromosomes 
+"""
+
+def randomParents()
+"""
+ Select Parents for tournament method
+ """
+
+def tournie()
+"""
+Tournament to generate new generation
+"""
+
+ 
