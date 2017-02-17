08f5a3c6b112f796619ebbfb2190572cb7a171f0
Charlotte Wilson <charlotteawilson.94@gmail.com>
2017-02-17 10:44:24 +0000

    settings tweaked: middleware higher up and whitelist for cors instead of allow all

--- cesGroundStation/settings.py
+++ cesGroundStation/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,12 +52,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'cesGroundStation.urls'
-CORS_ORIGIN_ALLOW_ALL = True 
+
+CORS_ORIGIN_WHITELIST = (
+
+   'localhost:8081',
+
+)
 
 TEMPLATES = [
     {
