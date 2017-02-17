8e296f7f92ef1a47dbe12379b699cdbcdbb69201
Charlotte Wilson <charlotteawilson.94@gmail.com>
2017-02-17 16:02:03 +0000

    inputs bootstrap

--- frontend/client/public/components/scheduler/controllers/scheduler.control.js
+++ frontend/client/public/components/scheduler/controllers/scheduler.control.js
@@ -2,6 +2,7 @@ scheduler
 	.controller('SchedulerController', function($scope, TLE){
 		TLE.query().$promise.then(function(data) {
 			$scope.tles = data;
+			$scope.message = "hello there ya tit";
 		});
 		
 });
