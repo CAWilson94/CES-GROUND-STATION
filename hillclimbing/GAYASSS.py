8a898e4cdd869aae7705d74c804c01abd02abd92
Charlotte Wilson <charlottead@live.co.uk>
2017-02-22 23:46:04 +0000

    basic pop and indi create, fitness check. start of actual GA

--- hillclimbing/GAYASSS.py
+++ hillclimbing/GAYASSS.py
@@ -10,6 +10,19 @@ class satPass:
 		self.endTime = endTime
 		self.duration = (endTime.minute- startTime.minute)
 
+
+class Individual:
+	' Class representing an individual'
+	def __init__(self, satList, fitness):
+		self.satList = satList
+		self.fitness = fitness
+
+	def setSatList(satList):
+		self.satList = satList
+
+	def setFitness(fitness):
+		self.fitness = fitness
+
 magicRainbow = satPass("Vegeta",(datetime.time(9, 0)),(datetime.time(9,20)));
 fabbyRainbow = satPass("Goku",(datetime.time(11,0)),(datetime.time(12,20)));
 greyRainbow = satPass("Yamcha",(datetime.time(10,10)),(datetime.time(10,30)));
@@ -46,9 +59,31 @@ def fitness(passList):
 		diff=(datetime.datetime.strptime(str(y.startTime),"%I:%M:%S")) - (datetime.datetime.strptime(str(z.endTime),"%I:%M:%S"))
 		#print("%s" %diff)
 		total += abs(int(diff.total_seconds()))
-	print ("YERMAWWWWWW\n")
-	print(str(datetime.timedelta(seconds=abs(total))))
 	fitness = total;
+	y.fitness = fitness # want fitness to be for the orders so create individual from this
 	return fitness
 
-fitness(orderOne)
+
+
+indiOne = Individual(orderOne,fitness(orderOne))
+indiTwo = Individual(ordertwo, fitness(ordertwo))
+
+indiList = [indiOne,indiTwo]
+
+def createPopulation(indiList): # will replace with db 
+	'will take in data from db instead and package individuals from there' 
+	population = []
+	for item in indiList:
+		population.append(item)
+	return population
+
+
+def GA(population):
+	for item in population:
+		print("fitness is:")
+		print(item.fitness)
+
+
+GA(createPopulation(indiList))
+
+
