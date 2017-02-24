de8273ec97040ebe472a0af039bbb88b26319788
Charlotte Wilson <charlotteawilson.94@gmail.com>
2017-02-24 19:21:34 +0000

    fitness basic working & tested: deleted duplicate modules & put code in correct place

--- GA/GA.py
+++ GA/GA.py
@@ -13,66 +13,28 @@ class satPass:
 		self.duration = (endTime.minute- startTime.minute)
 
 
-class Individual:
-	' Class representing an individual (or chromosome)'
-	def __init__(self, satList, fitness):
+class Gene:
+	' Class representing a gene in a chromosome'
+	def __init__(self, satList):
 		self.satList = satList
-		self.fitness = fitness
+		
 
 	def setSatList(satList):
 		self.satList = satList
 
-	def setFitness(fitness):
-		self.fitness = fitness
-# Just some test data ... nothing to see here.
-magicRainbow = satPass("Vegeta",(datetime.time(9, 0)),(datetime.time(9,20)));
-fabbyRainbow = satPass("Goku",(datetime.time(11,0)),(datetime.time(12,20)));
-greyRainbow = satPass("Yamcha",(datetime.time(10,10)),(datetime.time(10,30)));
-squeakyRainbow = satPass("Goten",(datetime.time(10,40)),(datetime.time(10,50)));
+class Population:
+	' Class representing population: a population contains chromosomes hurrah!'
 
-"""
-print (magicRainbow.name)
-print (magicRainbow.startTime)
-print (magicRainbow.endTime)
-print(magicRainbow.duration)
+	def __init__(self, indiList):
+		self.indiList = indiList
 
-print (fabbyRainbow.name)
-print (fabbyRainbow.startTime)
-print (fabbyRainbow.endTime)
-print(fabbyRainbow.duration)
-"""
+class Chromosome:
+	' Class representing a chromosome: where a chromosome is basically a random ordered list of sat passes'
+	def __init__(self, satPassList, fitness):
+		self.satPassList = satPassList
+		self.fitness = fitness # update later
 
-"""
-Need a list of orders
-iterate through each 
-the one with smallest time at end is winner
-"""
-orderOne = [magicRainbow, fabbyRainbow, greyRainbow, squeakyRainbow]
-ordertwo = [squeakyRainbow, fabbyRainbow, greyRainbow, magicRainbow]
-orderedPasses = [orderOne, ordertwo] # More test data .. more of nothing to see here.
-
-
-def fitness(passList):
-	""" The smallest time is the winner basically """
-	total = 0;
-	for y,z in zip(passList[1:],passList):
-		#print(y.name,z.name)
-		#print(y.startTime, z.endTime)
-		diff=(datetime.datetime.strptime(str(y.startTime),"%I:%M:%S")) - (datetime.datetime.strptime(str(z.endTime),"%I:%M:%S"))
-		#print("%s" %diff)
-		total += abs(int(diff.total_seconds()))
-	fitness = total;
-	y.fitness = fitness # want fitness to be for the orders so create individual from this
-	return fitness
-
-
-# May need to create class to package together the indiviuals. Or rather, map from db to object. 
-# This should be done with Django however. 
-indiOne = Individual(orderOne,fitness(orderOne))
-indiTwo = Individual(ordertwo, fitness(ordertwo))
-
-indiList = [indiOne,indiTwo] # TODO: This and the above should be in createPopulation? 
-
+'For use later on.. '
 def createPopulation(indiList): # will replace with db 
 	"""will take in data from db instead and package individuals from there"""
 	population = []
@@ -93,12 +55,25 @@ def randomParents(population):
 		clear(tempList)
 	return parents
 
+def fitness(passList):
+	""" The smallest time is the winner basically """
+	total = 0;
+	for y,z in zip(passList[1:],passList):
+		#print(y.name,z.name)
+		#print(y.startTime, z.endTime)
+		diff=(datetime.datetime.strptime(str(y.startTime),"%I:%M:%S")) - (datetime.datetime.strptime(str(z.endTime),"%I:%M:%S"))
+		#print("%s" %diff)
+		total += abs(int(diff.total_seconds()))
+	fitness = total;
+	y.fitness = fitness # want fitness to be for the orders so create individual from this
+	return fitness
+
 def tournie(population):
 	"""Tournament to generate new generation"""
 	newGen = []
 	crossed = []
 	while(len(crossed)!=len(population)):
-		newGen = randomParents(population) # TODO: create randomParents function
+		newGen = randomParents(population) 
 		i = random.randint(0,10) # Random int between 0 and 10
 		if(i < CROSSOVER_RATE):
 			tempIndiList = crossover(newGen[0], newGen[1])
@@ -110,7 +85,7 @@ def clear(someList):
 	"""python does not seem to have a clear list function..
 		Splice in the list [] (0 elements) at the location [:]
 		(all indexes from start to finish)
-	"""
+	"""	
 	someList[:] = []
 
 
@@ -120,18 +95,30 @@ def GA(population):
 	best = [] # Keep a list of the recent best solutions
 	gen = 0;
 	while(true):
-		gen++
+		gen+=1
 		sortByFitness(population)
 		population.append(best[0])
 		if(gen > 100): # since there is no definitive stopping value. i.e. if fitness was 0
 			sortByFitness(best)
 			print("best order is: " + best[0])
 			return
-	print("generation: " + gen + "best: " + population[0].chromosomeString) # TODO: create this chromosome string getter for population 
-	population = tournie(population) # TODO: Create tournie function		
+	print("generation: " + gen + "best: " + population[0].chromosomeString) # TODO: chromosomeString should be Gene string. 
+
+# Each one of these is a gene
+magicRainbow = satPass("Vegeta",(datetime.time(9, 0)),(datetime.time(9,20)));
+fabbyRainbow = satPass("Goku",(datetime.time(11,0)),(datetime.time(12,20)));
+greyRainbow = satPass("Yamcha",(datetime.time(10,10)),(datetime.time(10,30)));
+squeakyRainbow = satPass("Goten",(datetime.time(10,40)),(datetime.time(10,50)));
 
 
+orderOne = [magicRainbow, fabbyRainbow, greyRainbow, squeakyRainbow] # A chromosome basically (or individual)
+ordertwo = [squeakyRainbow, fabbyRainbow, greyRainbow, magicRainbow] # A chromosome basically (or individual)
+orderedPasses = [orderOne, ordertwo] # More test data .. more of nothing to see here.
 
-GA(createPopulation(indiList))
+chromo1 = Chromosome(orderOne, fitness(orderOne))
+chromo2 = Chromosome(ordertwo, fitness(ordertwo)) # TODO: get a more elegant way to do this ya pleb
+population = Population(orderedPasses)
 
 
+fitnessCompare = [chromo1.fitness, chromo2.fitness]
+print("best shceduled order is: ", min(fitnessCompare) )
