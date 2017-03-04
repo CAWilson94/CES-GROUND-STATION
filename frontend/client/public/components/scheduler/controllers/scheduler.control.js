53c7333665c837216f928c36cf4abd76fa6b8bfb
Charlotte Wilson <charlotteawilson.94@gmail.com>
2017-03-04 09:37:58 +0000

    need to get angular-datatables plugin for ng repeat to work with datatables

--- frontend/client/public/components/scheduler/controllers/scheduler.control.js
+++ frontend/client/public/components/scheduler/controllers/scheduler.control.js
@@ -1,6 +1,5 @@
 scheduler
 	.controller('SchedulerController', function($scope, TLE, AZEL){
-		$scope.hello = "hello tdddhere ya tit";
 		TLE.query().$promise.then(function(data) {
 			$scope.tles = data;
 		});
@@ -10,5 +9,7 @@ scheduler
 		
     	$('#tleTable').DataTable();	
     	$('.selectpicker').selectpicker();
+
+    	
 });
 
