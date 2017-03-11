9d56ee9b6225fe26dda3e366044ee59509615c6b
Charlotte Wilson <charlotteawilson.94@gmail.com>
2017-03-11 17:36:27 +0000

    material design dropdown for now: working data load in.

--- frontend/client/public/components/scheduler/controllers/scheduler.control.js
+++ frontend/client/public/components/scheduler/controllers/scheduler.control.js
@@ -1,14 +1,22 @@
 scheduler
 	.controller('SchedulerController', function($scope, TLE, AZEL,$timeout){
+
+    $scope.tle = null;
+    $scope.tles = null;
+    $scope.loadSat = function() {
+
 		TLE.query().$promise.then(function(data) {
 			$scope.tles = data;
 		});
+
+  };
+
 		AZEL.query().$promise.then(function(date){
 			$scope.azel = data;
 		});
 
 
-		$scope.user = null;
+	$scope.user = null;
   $scope.users = null;
 
   $scope.loadUsers = function() {
