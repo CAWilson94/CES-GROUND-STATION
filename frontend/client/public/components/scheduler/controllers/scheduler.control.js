e309abfda0157bbcab1bfda1fc019a2372ee7f22
Charlotte Wilson <charlotteawilson.94@gmail.com>
2017-03-11 18:37:11 +0000

    priority list added in with scope output for testing

--- frontend/client/public/components/scheduler/controllers/scheduler.control.js
+++ frontend/client/public/components/scheduler/controllers/scheduler.control.js
@@ -14,34 +14,13 @@ scheduler
 		AZEL.query().$promise.then(function(date){
 			$scope.azel = data;
 		});
-
-
-	$scope.user = null;
-  $scope.users = null;
-
-  $scope.loadUsers = function() {
-
-    $scope.users =  $scope.users  || [
-        { id: 1, name: 'TEABAGS' },
-        { id: 2, name: 'DOOPPYY' },
-        { id: 3, name: 'BALALALLA' },
-        { id: 4, name: 'ROASTERS' },
-        { id: 5, name: 'CUBESAT1' },
-        { id: 1, name: 'YERMAW' },
-        { id: 2, name: 'LINDAAA' },
-        { id: 3, name: 'BALDEEPSMAW' },
-        { id: 4, name: 'COFFEE' },
-        { id: 5, name: 'COOKIES' }
+   
+  
+     $scope.sizes = [
+          "1: low",
+          "2: default",
+          "3: high",
       ];
-  };
-
-
-
-    
-		$scope.button_click = function() {
-          window.alert("boopity");
-      };
-
 		
     	
 });
