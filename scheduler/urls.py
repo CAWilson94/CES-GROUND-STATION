0b868c86c70696037e09058264b7500951d39bb2
Adam Kidd <ksb12177@uni.strath.ac.uk>
2017-03-22 11:30:59 +0000

    .getlist[] method object is not subscriptable

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -15,6 +15,8 @@ urlpatterns = [
 # 	url(r'^scheduler/?$', views.TLEList.as_view()),
 # 	url(r'^scheduler/(?P<pk>[0-9]+)/?$', views.TLEDetail.as_view()),
  	url(r'^tles/(?P<pk>[0-9]+)/azel/?$',views.PyephemData.as_view()),
+ 	url(r'^postdata/(?P<nameArray>[a-z]+)/?$',views.postEx.as_view()),
+ 	url(r'^postdata/?$',views.postEx.as_view()),
  ]
 
 # urlpatterns = format_suffix_patterns(urlpatterns)
