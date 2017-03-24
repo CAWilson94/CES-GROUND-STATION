1261fcd56b0011caf798ae4fe2b3dc994e649acc
Charlotte Wilson <charlotteawilson.94@gmail.com>
2017-03-24 09:58:03 +0000

    error and success callbacks for get requests and some documenting

--- frontend/client/public/components/scheduler/controllers/scheduler.control.js
+++ frontend/client/public/components/scheduler/controllers/scheduler.control.js
@@ -3,15 +3,26 @@ scheduler
 
     $scope.tle = null;
     $scope.tles = null;
-    // Loading required data into dropdown; can take a few ms
 
+    /**
+     * Load in TLE data from Django side
+     * @return {[type]} [description]
+     */
     $scope.loadSat = function() {
       // Grab TLE from Django 
       TLE.query().$promise.then(function(data) {
-        $scope.tles = data;
-      });
+          $scope.tles = data;
+        },
+        function errorCallBack(response) {
+          console.log(response.status + " : " + response.statusText);
+        },
+
+      );
     };
 
+    /**
+     * Load in azimuth and elevation values from Django Models
+     */
     $scope.loadAxel = function() {
       // Grabbing AZEL data from Django; under construction
       AZEL.query().$promise.then(function(date) {
@@ -19,6 +30,7 @@ scheduler
       });
     };
 
+    // priorities: should default at 2 in dropdown 
     $scope.priorities = [{
       name: "low",
       priority: 1,
@@ -37,12 +49,11 @@ scheduler
      * SatMission object: sat name and priority
      * SatMissions List: containing sat missions objects
      */
-    var missions
 
-    $scope.hallo = "hello";
     // update table from drop down; will need to take in data from scheduler i.e. table dropdown --> fetch schedule data based on this then 
     // update table from scheduled data
 
+    // Not in use currently: should default to JSON but in case we need them? 
     var config = {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
@@ -60,33 +71,30 @@ scheduler
         $scope.name.push($scope.tle.name)
         $scope.pri.push($scope.priority.priority)
 
-        try {
-          // Not sure this try should be here, try for post 
-
-          $http.post('http://127.0.0.1:8000/api/missions/', $scope.name)
-            .then(function successCallBack(response) {
-                // Succeess is anything between 200 and 299
-                console.log("yasss")
-              },
-              function errorCallBack(response) {
-                // called asynchronously if an error occurs
-                // or server returns response with an error status.
-                // Error is anything outside of range previously mentioned. 
-                console.log(response.status + " : " + response.statusText);
-              });
-
-        } catch (err) {
-          console.log("Post went wrong..")
-        }
+        // Not sure this try should be here, try for post 
+        $http.post('http://127.0.0.1:8000/api/missions/', $scope.name)
+          .then(function successCallBack(response) {
+              // Succeess is anything between 200 and 299
+              console.log(response)
+            },
+            function errorCallBack(response) {
+              // called asynchronously if an error occurs
+              // or server returns response with an error status.
+              // Error is anything outside of range previously mentioned. 
+              console.log(response.status + " : " + response.statusText);
+            },
+            // Uncomment for testing output or use django shell to check missions model contents
+            alert($scope.name)
+          );
 
       } catch (err) {
         alert("you must first select a satellite and a priority")
       }
 
-
-
     };
 
 
 
+
+// End of controller please leave it alone.
   });
