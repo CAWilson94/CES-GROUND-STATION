13f18fa8dbe3543f0a3763904c72edd572c36c02
Baldeep <skb12156@uni.strath.ac.uk>
2017-03-25 22:16:01 +0000

    began set up of celery, completed first steps

---
+++ cesGroundStation/celery.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import #, unicode_literals
+import os
+from celery import Celery
+
+# Setting celery config file to the Django settings file
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','cesGroundStation.settings')
+
+# One instance of Celery created for our app
+app = Celery('cesGroundStation')
+
+# Celery config starts with "CELERY_" in django settings.py
+app.config_from_object('django.conf:settings', namespace="CELERY")
+
+# Tells celery to look for the tasks.py file in each app
+# Alternatively the CELERY_IMPORTS setting has to be set
+app.autodiscover_tasks(lambda: settings:INSTALLED_APPS)
+
+@app.task(bind = True)
+def debug_task(self):
+	print('Request: {0!r}'.format(self.request))
---
+++ scheduler/tasks.py
@@ -0,0 +1,35 @@
+from celery import task
+
+
+#app = Celery('tasks.py', broker='amqp://localhost')
+
+@task()
+def pollForNew():
+		print ( "Polling for new")
+		try:
+			mission_list = ms.findMissionsByStatus("New")
+			for i in mission_list:
+				i.status = ("Waiting")
+				print("Count = %r" %i)
+				pass
+		except TLE.DoesNotExist as e:
+			print("Already exists")	 
+
+
+@task()
+def pollQueue():
+		count = 0
+		while (count < 10):
+			try:
+				mission_list = ms.findMissionsByStatus("Ready")
+				for i in mission_list:
+					i.status = ("Tracked")
+				print("Count = %r" %count)
+				count+=1
+				pass
+			except TLE.DoesNotExist as e:
+				print("Already exists")	
+				 
+@task()
+def reverse(string):
+	return string[::-1]
