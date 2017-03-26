3d4e6d7e86af7f95b2b33838605008145e89de0b
charlottewilson <charlotteawilson.94@gmail.com>
2017-03-26 18:58:40 +0100

    fot item in azel list: set position of rotator for that given az and el

--- scheduler/rotatorController.py
+++ scheduler/rotatorController.py
@@ -1,12 +1,12 @@
-from RotatorServices import rotator_services
-import services
+from scheduler.RotatorServices import rotator_services
+from scheduler.services import Services
 from datetime import date, datetime, timedelta
-from models import NextPass
+from scheduler.models import NextPass, TLE
 
 
-class rotator_controller(NextPass):
+class rotator_controller():
     """Controller for rotators:
-                Take in next pass object which 
+                Take in next pass object which
                 contains TLE object and start and end times of the pass
     """
 
@@ -15,20 +15,12 @@ class rotator_controller(NextPass):
         self.arg = arg
 
     def moveRotators(tleEntry):
-        """ 
-                From the AzelForEachSecond function: send commands to rotators to move them. 
         """
-        azel = services.getAzElForPeriod(tleEntry, NextPass.riseTime,
+                From the AzelForEachSecond function: send commands to rotators to move them.
+        """
+        s = Services()
+
+        azel = Services.getAzElForPeriod(self, tleEntry, NextPass.riseTime,
                                          NextPass.setTime, NextPass.period)
-        rs.set_position(azel.elevation, azel.azimuth)
-
-
-tleEntry = TLE('0', 'PROMETHEUS 2-3',
-               '1 41855U 16067H   17017.17439223 -.00000035  00000-0  22910-5 0  9998',
-               '2 41855  97.9660  92.6242 0012070  83.3470 276.9124 14.95885483  9917', )
-
-azelList = Services.getAzElForPeriod(self, tleEntry, datetime(2017, 6, 6, 12, 2, 42),
-                                     datetime(2017, 6, 6, 12, 9, 2), 30)
-
-for item in azel:
-    print(item.elevation + " : is elevation ")
+        for item in azel:
+            rs.set_position(item.elevation, item.azimuth)
