12c17296d25d3491200892461f229648272a0235
ek12139 <ekb12139@uni.strath.ac.uk>
2017-03-30 10:22:43 +0100

    Added better error msg

--- scheduler/rotatorController.py
+++ scheduler/rotatorController.py
@@ -22,7 +22,7 @@ class rotator_controller():
     try:
         ser = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=2)
     except:
-        print("Port Not Detected")
+        print("Port Not Detected. Please connect rotators.")
     #changed to COM8 for Robbies Laptop
     #/dev/ttyACM0 for linux
 
