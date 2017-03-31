922c00373c20dbb99034a59f6a20cf495829004f
charlottewilson <charlotteawilson.94@gmail.com>
2017-03-31 23:13:13 +0100

    moved GA to relevant folder: deleted some function test files no longer used

--- scheduler/MOT/GAScheduler.py
+++ scheduler/MOT/GAScheduler.py
@@ -3,12 +3,23 @@ from datetime import timedelta
 from scheduler.models import NextPass
 from scheduler.schedulerHelper import SchedulerHelper
 from scheduler.MOT.schedulerInterface import MOT
-from scheduler import GA as ga
+from . import GA as ga
+from scheduler.models import Mission
 
 
 class MOTRuleBased(MOT):
 
+missions = Mission.objects.all()
         # Returns a list of passes with no conflicts
     def find(self, missions, usefulTime):
+        """
+
+        Find a list of passes with no conflicts
+        """
+
+        passes = SchedulerHelper.getPassesFromMissions(self, missions)
+        print("Passes: " + str(len(passes)))
 
         return orderOfPasses
+
+find(missions, 100)
