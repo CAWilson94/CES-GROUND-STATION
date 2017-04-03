cc04ba1e13021e8858ad56b5a1c8c7bcc92bda72
charlottewilson <charlotteawilson.94@gmail.com>
2017-04-03 19:55:37 +0100

    unused file

--- scheduler/MOT/geneticAlgorithm.py
+++
@@ -1,8 +0,0 @@
-from scheduler.MOT.schedulerInterface import MOT
-
-# Class implements the MOT interface from schedulerInterface.py
-class MOTGeneticAlgorithm(MOT):
-
-	# usefulTime is an integer used by the Hill Climber can ignore it in your implementation
-	def find(self, missions, usefulTime):
-		pass
