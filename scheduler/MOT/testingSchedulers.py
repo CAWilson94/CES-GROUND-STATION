7d27b6edb01d82f0ef7fa7ee67f95ae81644d8da
charlottewilson <charlotteawilson.94@gmail.com>
2017-04-04 12:10:35 +0100

    total contact and non contact time for sat passes in seconds

--- scheduler/MOT/testingSchedulers.py
+++ scheduler/MOT/testingSchedulers.py
@@ -4,3 +4,13 @@ def test(passes):
     end_pass = passes.last().setTime
     duration = end_pass - first_pass
     print("DURATION: ----------------" + str(duration))
+
+    total_contact_time = 0
+    for item in passes:
+        total_contact_time += item.duration.seconds
+
+    total_non_contact_time = duration.seconds - total_contact_time
+
+    print("TOTAL CONTACT TIME ======" + str(total_contact_time) + " IN SECONDS")
+
+    print("TOTAL NON CONTACT TIME ======" + str(total_non_contact_time) + " IN SECONDS")
